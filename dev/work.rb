439f3d35dfd7ea33cc3f56f97c981ea91b96942a
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-01-17 21:25:07 -0500

    dev

--- dev/work.rb
+++ dev/work.rb
@@ -1,25 +1,10 @@
-class TxWorkflow < NiceAssets::AssetWorkflow
-  def source_spec
-    source_assoc = SelfishAssociations::Associations::HasOne.new("source", Service, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false)
-    source_spec = NiceAssets::AssetSpecification.new(source_assoc)
-  end
+Object.send(:remove_const, :TranscriptionWorkflow) if defined?(TranscriptionWorkflow)
+class TranscriptionWorkflow < NiceAssets::AssetWorkflow
+  owned_by Service
 
-  def tx_spec
-    tx_assoc = SelfishAssociations::Associations::HasOne.new("transcribed_transcript", Service)
-    tx_spec = NiceAssets::AssetSpecification.new(tx_assoc)
-  end
-
-  def asset_roster
-    roster = NiceAssets::AssetRoster.new
-    roster.add_spec(:source, source_spec)
-    roster.add_spec(:transcript, tx_spec)
-    return roster
-  end
+  asset :source, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false
+  asset :audio, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "DspAudio"
+  asset :asr, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :audio, class_name: "AsrOutput"
+  asset :stream, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "StoeStream"
+  asset :transcript, after: [:asr, :stream], class_name: "TranscribedTranscript", as: "output"
 end
-
-graph = NiceAssets::AssetGraph.new
-graph.add_node(:source)
-graph.add_node(:audio, after: :source)
-graph.add_node(:stream, after: :audio)
-graph.add_node(:asr, after: :audio)
-graph.add_node(:tx, after: [:asr, :stream])
