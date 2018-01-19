ec181083f1bdbe1b6579a32d85598c49f1092f11
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-01-19 16:40:38 -0500

    dev

--- dev/work.rb
+++ dev/work.rb
@@ -6,7 +6,7 @@ class TranscriptionWorkflow < NiceAssets::AssetWorkflow
   asset :audio, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "DspAudio"
   asset :asr, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :audio, class_name: "AsrOutput"
   asset :stream, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "StoeStream"
-  asset :transcript, after: [:asr, :stream], class_name: "TranscribedTranscript", as: "output"
+  asset :transcript, after: [:asr, :stream], class_name: "TranscribedTranscript"
 
   outputs :transcript
 
