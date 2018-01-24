7e350657d69cd8600807b96d4db56b7feffe7271
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-01-23 20:56:57 -0500

    dev

--- dev/work.rb
+++ dev/work.rb
@@ -3,7 +3,8 @@ class TranscriptionWorkflow < NiceAssets::AssetWorkflow
   owned_by Service
 
   asset :source, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false
-  asset :audio, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "DspAudio"
+  checkpoint :audio_input_ready?
+  asset :audio, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :audio_input_ready?, class_name: "DspAudio"
   asset :asr, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :audio, class_name: "AsrOutput"
   asset :stream, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "StoeStream"
   asset :transcript, after: [:asr, :stream], class_name: "TranscribedTranscript"
@@ -11,11 +12,15 @@ class TranscriptionWorkflow < NiceAssets::AssetWorkflow
 
   outputs :transcript, :caption
 
-  def request_asset(asset)
-    puts "DEBUG: requesting #{asset}"
+  def request_asset(label)
+    puts "DEBUG: requesting #{label}"
   end
 
   def ignore_caption?
     Random.rand < 0.5
+  end
+
+  def audio_input_ready?
+    asset_ready?(:source) || asset_ready?(:stream)
   end
 end
