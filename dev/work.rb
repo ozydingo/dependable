8fded53d6d7eeeefadc68738d277919b32f20ffa
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-02-24 14:02:47 -0500

    dev

--- dev/work.rb
+++ dev/work.rb
@@ -24,3 +24,19 @@ class TranscriptionWorkflow < NiceAssets::AssetWorkflow
     asset_ready?(:source) || asset_ready?(:stream)
   end
 end
+
+Service
+class Service < ActiveRecord::Base
+  extend NiceAssets::ActiveWorkflow
+  self::NiceWorkflow = nice_assets do
+    asset :source, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false
+    checkpoint :audio_input_ready?
+    asset :audio, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :audio_input_ready?, class_name: "DspAudio"
+    asset :asr, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :audio, class_name: "AsrOutput"
+    asset :stream, ->(s){ where media_file_id: s.media_file_id }, foreign_key: false, after: :source, class_name: "StoeStream"
+    asset :transcript, after: [:asr, :stream], class_name: "TranscribedTranscript"
+    asset :caption, ->(s){ where threeplay_transcript_id: s.output_transcript.id }, foreign_key: false, after: :transcript, class_name: "ThreeplayCaption", ignore: :ignore_caption?
+
+    outputs :transcript, :caption
+  end
+end
